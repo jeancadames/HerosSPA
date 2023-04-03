7095721f4db763a5de0cc6d0e569ad6e5d5b7db0
Jean Adames <103436725+jeancadames@users.noreply.github.com>
2023-04-02 22:12:16 -0400

    Update HeroList.jsx

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -7,7 +7,7 @@ export const HeroList = ({publisher}) => {
     const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]); 
 
   return (
-    <div className="row rows-cols-1 row-cols-md-3 g-3">
+    <div className="row rows-cols-1 row-cols-md-3 g-3 list-container">
           {heroes.map((heroe) => 
           
             <HeroCard key={heroe.id}
